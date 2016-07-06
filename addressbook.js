5f6a3dc1693b01804844adb15d2ba33f32d01a2e
Shreya Kaicker <shreyakaicker@gmail.com>
2016-07-06 23:38:07 +0000

    add done

--- addressbook.js
+++ addressbook.js
@@ -1,1 +1,38 @@
+  //## Address book
   
+  
+  function Person(firstName,lastName) {
+      
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.emails = [];
+    
+  }
+  
+  function Email(address,type) {
+      this.address = address;
+      
+       if (type === "work") {this.type =  "work" ;} 
+          else if (type === "home") {this.type = "home" ;}
+          else  {this.type = "other" ;}
+          
+         
+          
+  }
+  
+  Person.prototype = {
+      addEmail: function(address,type) {
+        
+        this.emails.push(new Email(address, type))
+        
+        }
+      }
+      
+      var shreya = new Person ("Shreya","Kaicker")
+      var ziad = new Person("Ziad","Saab")
+     
+      console.log(shreya,ziad);
+      shreya.addEmail("shreyakaicker@gmail.com","work")
+      console.log(shreya);
+     
+
