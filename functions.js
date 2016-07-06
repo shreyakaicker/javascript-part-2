480e3a2b94aa82fd5166fd376a48033dd67a1ff5
Shreya Kaicker <shreyakaicker@gmail.com>
2016-07-06 18:57:46 +0000

    qs 2 done

--- functions.js
+++ functions.js
@@ -22,3 +22,35 @@ items.sort(function (a, b) {
 
 
 
+/*Create an array of objects (don’t need to use new here, just regular object literals). These objects will contain a name and email property. Then, run some code that will sort your array by the longest name. Then, run some code that will sort your array by e-mail address in alphabetical order.*/
+var info = [{ name : "Shreya" , email : "shreyakaicker@gmail.com" } , {name : "Pran" , email : "kaicker@gmail.com"} ,{name : "Dunmmmmmmnm" , email : "aickkdkdkkdder@gmail.com"} ]
+
+info.sort(function (a, b) {
+  if (a.name.length > b.name.length) {
+      return -1 ;
+  }
+  else if (a.name.length < b.name.length) {
+      return 1 ;
+  }
+  else {
+      return 0 ;
+  }
+}); 
+
+console.log(info)
+
+
+
+info.sort(function (a, b) {
+  if (a.email > b.email) {
+      return 1 ;
+  }
+  else if (a.email < b.email) {
+      return -1 ;
+  }
+  else {
+      return 0 ;
+  }
+});
+
+console.log(info)
