0658cb76c1f998767aba3f9cbb190105533182f3
Shreya Kaicker <shreyakaicker@gmail.com>
2016-07-07 00:41:42 +0000

    left

--- map.js
+++ map.js
@@ -18,7 +18,19 @@ function Map(width,height) {
     
     this.width = width;
     this.height = height;
-    this.tiles = ;
+    
+    var array1 = [];
+    for(var i=0; i < width; i++) {
+        var array2 = [];
+        
+        for(var x=0; i < height; i++) {
+            var tile = new Tile()
+            array2.push(tile)
+        }
+        array1.push(array2) ;
+    }
+        
+   this.tiles = array1 ;
     
    
 }
